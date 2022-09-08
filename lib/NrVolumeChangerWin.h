6f35e5434aa0e32c7ed6ac26827f3c0a04e9fb1f
Francesco Lamonica <lamonica@netresults.it>
2022-09-08 16:59:54 +0200

    added missing windows include file

---
+++ lib/NrVolumeChangerWin.h
@@ -0,0 +1,25 @@
+#ifndef NRVOLC_WIN_IMPL_H
+#define NRVOLC_WIN_IMPL_H
+
+#include <VolumeChanger.h>
+
+class AudioObjectPropertyAddress;
+
+class NRVOLC_LIB_EXPORT NrVolumeChangerWinImpl : public NrVolumeChanger
+{
+    int getDefaultInputDeviceId() const;
+    int getDefaultOutputDeviceId() const;
+
+    int setInputDeviceVolume(int devId, double percent);
+    double getInputDeviceVolume(int devId) const;
+    int setOutputDeviceVolume(int devId, double percent);
+    double getOutputDeviceVolume(int devId) const;
+public:
+    NrVolumeChangerWinImpl(QObject *parent=nullptr);
+    virtual int setDefaultInputVolume(double percent);
+    virtual double getDefaultInputVolume() const;
+    virtual int setDefaultOutputVolume(double percent);
+    virtual double getDefaultOutputVolume() const;
+};
+
+#endif

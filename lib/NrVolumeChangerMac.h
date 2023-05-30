f3502c40da2c7f6d30055a7432edb14bccf7e5c1
Francesco Lamonica <lamonica@netresults.it>
2023-05-31 01:05:26 +0200

    fixes for macOS devices

--- lib/NrVolumeChangerMac.h
+++ lib/NrVolumeChangerMac.h
@@ -1,9 +1,11 @@
 #ifndef NRVOLC_COCOA_WRAPPER_H
 #define NRVOLC_COCOA_WRAPPER_H
 
+#include "CoreAudio/AudioHardware.h"
+//#include "CoreAudio/AudioHardwareDeprecated.h"
 #include "VolumeChanger.h"
 
-class AudioObjectPropertyAddress;
+//class AudioObjectPropertyAddress;
 
 class NRVOLC_LIB_EXPORT NrVolumeChangerMacImpl : public NrVolumeChanger
 {
@@ -14,11 +16,12 @@ class NRVOLC_LIB_EXPORT NrVolumeChangerMacImpl : public NrVolumeChanger
     double getInputDeviceVolume(int devId) const;
     int setOutputDeviceVolume(int devId, double percent);
     double getOutputDeviceVolume(int devId) const;
+    AudioDeviceID getDeviceID(std::string uid) const;
     //TODO make these two templated
     int setDeviceProperty(int devId, AudioObjectPropertyAddress *propAddr, double propValue);
     double getDeviceProperty(int devId, AudioObjectPropertyAddress *propAddr) const;
 public:
-    NrVolumeChangerMacImpl(QObject *parent=nullptr);
+    NrVolumeChangerMacImpl();
     virtual int setDefaultInputVolume(double percent);
     virtual double getDefaultInputVolume() const;
     virtual int setDefaultOutputVolume(double percent);
